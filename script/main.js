bf737da1205f161ebcf15aa2714210a7aa19e7d2
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-09 11:21:36 +0100

    Start

---
+++ script/main.js
@@ -0,0 +1,15 @@
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
+
+
+// DESCRIZIONE:
+// RI-Creare la navbar con i vari dropdown del sito: https://www.revolut.com/en-IT
+// I dropdown però appaiono e scompaiono al click e non al rollover.
+// Cerco poi di gestire il fatto che possa essere aperto 1 solo dropdown alla volta.
+//
+// NOTE:
+// quindi l’ex base ha: sia l’interazione dei dropdown (però sul click),
+// sia l’impaginazione e grafica uguali al sito;
+// prima di buttarvi su altri bonus, il primo bonus è: completare la navbar anche in ottica
+// responsive con relativo hamburger menù.
