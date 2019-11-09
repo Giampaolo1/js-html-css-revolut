ba29d70be7ed62d54be3602e4328fb50388563b8
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-09 20:08:54 +0100

    a

--- script/main.js
+++ script/main.js
@@ -1,8 +1,3 @@
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-// });
-
-
 // DESCRIZIONE:
 // RI-Creare la navbar con i vari dropdown del sito: https://www.revolut.com/en-IT
 // I dropdown però appaiono e scompaiono al click e non al rollover.
@@ -13,3 +8,29 @@
 // sia l’impaginazione e grafica uguali al sito;
 // prima di buttarvi su altri bonus, il primo bonus è: completare la navbar anche in ottica
 // responsive con relativo hamburger menù.
+
+
+$( document ).ready(function() {
+
+  $(".dropdown").click(function(){
+    $(".dropdowncontent").toggle();
+
+
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
